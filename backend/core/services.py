c763be3f00b0b8d250879b62e31e9792e2f5bb34
Khudadad Khawari <54738188+KhudadadKhawari@users.noreply.github.com>
2026-09-08 16:56:52 +0430

    Fix backend lint

--- backend/core/services.py
+++ backend/core/services.py
@@ -6,7 +6,14 @@ from django.core.serializers.json import DjangoJSONEncoder
 from django.db import transaction
 from django.utils import timezone
 
-from .models import AuditLog, ImpersonationSession, PamirPermission, Role, Tenant, TenantMembership
+from .models import (
+    AuditLog,
+    ImpersonationSession,
+    PamirPermission,
+    Role,
+    Tenant,
+    TenantMembership,
+)
 
 
 TENANT_ADMIN_ROLE_NAME = "Tenant Admin"
